ec3f7d3fc3ff0460fc618c9dc416c39ca5ce98db
JuanSanchez39 <juan_sanchez39@hotmail.com>
2023-08-03 21:08:26 -0400

    started on script.js

---
+++ Assets/script.js
@@ -0,0 +1,6 @@
+var key = '9e3433936b6e90baa334345a1d72419a';
+var city = "Miami"
+
+//Grabs the current time and date
+var date = moment().format('dddd, MMMM Do YYYY');
+var dateTime = moment().format('YYYY-MM-DD HH:MM:SS')
